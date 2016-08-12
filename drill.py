d7fc1d3a7fe809f64482fb4713ef52b372f7b49e
mnigul <martinnigul@hotmail.com>
2016-08-12 22:23:50 +0300

    Add files via upload
    Completed program, that will automatically read old file, correct the coorinates, and then write them in a new file in original form.

--- drill.py
+++ drill.py
@@ -1,7 +1,7 @@
 failiNimi = "drill.ngc"
 uusFail = "drillUus.ngc"
 
-#korjab ridadest  vastava tekstid
+#korjab ridadest vastavad tekstid
 def kogumine(tekst,line):
     return tekst + line
 
@@ -31,6 +31,54 @@ def failistLugemine(failiNimi):
     f.close
     return tekstid, punktid
 
+#leiab unikaalsete x loendi (leiab ainult eri x-koordinaatide arvud, korduvaid ei loe)
+def leiabxUnique(x):
+    xUnique = []
+    for px in x:
+        if not px in xUnique:
+             xUnique.append(px)
+    return xUnique
+
+#parsib punktiderea punktideks
+def punktiridaPunktideks(punktid, too):
+    xyText = punktid[too].split('\n')
+    x = []
+    xy = []
+    for i in range (0,len(xyText)-1):
+        x.append(xyText[i].split(' ')[0])
+        xy.append(xyText[i].split(' '))
+    return xy, leiabxUnique(x)
+    
+#muudab tagasi/tagasi järjestuse tagasi/edasi 
+def sorteerija(xUnique, xy):
+    xyUus = []
+    for i in range(0,len(xUnique)):
+        pp = [k for k in xy if xUnique[i] in k]
+        if i % 2 > 0:
+            pp.sort()
+        for pxy in pp:
+            xyUus.append(pxy)	
+    return xyUus
+
+def punktideKorrigeerimine(punktid):
+    punktidUus = []
+    for i in range (0,len(punktid)):
+        xy = []
+        xyUus = []
+        xUnique = []
+        [xy, xUnique] = punktiridaPunktideks(punktid, i)
+        xyUus = sorteerija(xUnique, xy)
+        punktidUus.append(liidabPunktid(xyUus))
+    punktidUus.append(" ")
+    return(punktidUus)   
+
+#Liidab punktid tagasi stringiks
+def liidabPunktid(xyUus):
+    punktUus = ""
+    for i in range(0,len(xyUus)):
+        punktUus = punktUus + xyUus[i][0] + " " + xyUus[i][1] + '\n'
+    return punktUus
+
 #kustutab tulemuse faili tyhjaks ja kirjutab uue tulemuse faili 
 def failiKirjutamine(tekstid, failiNimi):
     f = open(failiNimi, 'w')
@@ -40,4 +88,5 @@ def failiKirjutamine(tekstid, failiNimi):
     f.close
           
 [tekstid, punktid] = failistLugemine(failiNimi)
+uusPunktid = punktideKorrigeerimine(punktid)
 uusFail = failiKirjutamine(tekstid, uusFail)
